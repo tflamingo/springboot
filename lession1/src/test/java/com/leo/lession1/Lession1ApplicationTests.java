4a9be3decdefd70b319077fb5bcbbfc77762cdd4
tflamingo <tflamingo@163.com>
2019-11-11 23:10:53 +0800

    add lession1

---
+++ lession1/src/test/java/com/leo/lession1/Lession1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.lession1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lession1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
